7a7eb2b55f8be8111aa0535719f89a60c5c616ba
KS Chan <mrkschan@gmail.com>
2015-11-02 23:01:43 +0800

    Support parsing config from uwsgibeat.yml

--- config.go
+++ config.go
@@ -1,6 +1,18 @@
 package main
 
 type UWSGIConfig struct {
+	// URL to uWSGI stats server.
+	// Defaults to "127.0.0.1:1717"
+	URL string
+
+	// The method used to dial to uWSGI stats server.
+	// It can be either "tcp", "unix", or "http".
+	// Defaults "tcp".
+	Method string
+
+	// Period defines how often to read stats in seconds.
+	// Defaults to 1 second.
+	Period *int64
 }
 
 type ConfigSettings struct {
--- uwsgibeat.go
+++ uwsgibeat.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"time"
+	"net/url"
+
 	"github.com/elastic/libbeat/beat"
+	"github.com/elastic/libbeat/cfgfile"
+	"github.com/elastic/libbeat/logp"
 )
+
+const selector = "uwsgibeat"
 
 // UWSGIbeat implements Beater interface and sends uWSGI status using libbeat.
 type UWSGIbeat struct {
-	config ConfigSettings
+	// UbConfig holds configurations of UWSGIbeat parsed by libbeat.
+	UbConfig ConfigSettings
+
+	url    *url.URL
+	method string
+	period time.Duration
 }
 
 // Config UWSGIbeat according to uwsgibeat.yml.
 func (ub *UWSGIbeat) Config(b *beat.Beat) error {
-	return nil
+		err := cfgfile.Read(&ub.UbConfig, "")
+		if err != nil {
+			logp.Err("Error reading configuration file: %v", err)
+			return err
+		}
+
+		var u string
+		if ub.UbConfig.Input.URL != "" {
+			u = ub.UbConfig.Input.URL
+		} else {
+			u = "127.0.0.1:1717"
+		}
+		ub.url, err = url.Parse(u)
+		if err != nil {
+			logp.Err("Invalid uWSGI stats server address: %v", err)
+			return err
+		}
+
+		if ub.UbConfig.Input.Method != "" {
+			ub.method = ub.UbConfig.Input.Method
+		} else {
+			ub.method = "tcp"
+		}
+
+		if ub.UbConfig.Input.Period != nil {
+			ub.period = time.Duration(*ub.UbConfig.Input.Period) * time.Second
+		} else {
+			ub.period = 1 * time.Second
+		}
+
+		logp.Debug(selector, "Init uwsgibeat")
+		logp.Debug(selector, "Watch %v", ub.url)
+		logp.Debug(selector, "Method %v", ub.method)
+		logp.Debug(selector, "Period %v", ub.period)
+
+		return nil
 }
 
 // Setup UWSGIbeat.
