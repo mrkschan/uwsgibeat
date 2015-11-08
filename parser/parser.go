061511efb870bd466e48d5eb7eda20d6898d8d99
KS Chan <mrkschan@gmail.com>
2015-11-09 00:21:02 +0800

    Support using TCP to get stats from uWSGI

--- parser/parser.go
+++ parser/parser.go
@@ -1,9 +1,18 @@
 package parser
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/url"
+)
+
 // Parser parses stats from uWSGI server.
 type Parser interface {
-	// Parse stats from the given url.
-	Parse(url string) (map[string]interface{}, error)
+	// Parse stats from the given URL.
+	Parse(url url.URL) (map[string]interface{}, error)
 }
 
 // StatsParser is a Parser that parses uWSGI stats server.
@@ -15,7 +24,75 @@ func NewStatsParser() Parser {
 	return &StatsParser{}
 }
 
-// Parse uWSGI stats from given url.
-func (p *StatsParser) Parse(url string) (map[string]interface{}, error) {
-	return nil, nil
+// Parse uWSGI stats from the given URL.
+func (p *StatsParser) Parse(url url.URL) (map[string]interface{}, error) {
+	var reader io.Reader
+
+	switch url.Scheme {
+	case "tcp":
+		conn, err := net.Dial(url.Scheme, url.Host)
+		if err != nil {
+			return nil, err
+		}
+		defer conn.Close()
+		reader = conn
+
+	default:
+		return nil, fmt.Errorf("%v is a unsupported protocol", url.Scheme)
+	}
+
+	// uWSGI stats is expected to be a JSON document.
+	// Ref - http://uwsgi-docs.readthedocs.org/en/latest/StatsServer.html
+	body, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	var payload map[string]interface{}
+	if err := json.Unmarshal([]byte(body), &payload); err != nil {
+		return nil, err
+	}
+
+	// Convert float to int values.
+	stats := ftoi(payload)
+
+	return stats, nil
+}
+
+// ftoi returns a copy of in where float values are casted to int values.
+func ftoi(in map[string]interface{}) map[string]interface{} {
+	out := map[string]interface{}{}
+
+	for k, v := range in {
+		switch v.(type) {
+		case float64:
+			vt := v.(float64)
+			out[k] = int(vt)
+		case map[string]interface{}:
+			vt := v.(map[string]interface{})
+			out[k] = ftoi(vt)
+		case []interface{}:
+			vt := v.([]interface{})
+			l := len(vt)
+			a := make([]interface{}, l)
+			for i := 0; i < l; i++ {
+				e := vt[i]
+				switch e.(type) {
+				case float64:
+					et := e.(float64)
+					a[i] = int(et)
+				case map[string]interface{}:
+					et := e.(map[string]interface{})
+					a[i] = ftoi(et)
+				default:
+					a[i] = e
+				}
+			}
+			out[k] = a
+		default:
+			out[k] = v
+		}
+	}
+
+	return out
 }
--- uwsgibeat.go
+++ uwsgibeat.go
@@ -6,7 +6,11 @@ import (
 
 	"github.com/elastic/libbeat/beat"
 	"github.com/elastic/libbeat/cfgfile"
+	"github.com/elastic/libbeat/common"
 	"github.com/elastic/libbeat/logp"
+	"github.com/elastic/libbeat/publisher"
+
+	"github.com/mrkschan/uwsgibeat/parser"
 )
 
 const selector = "uwsgibeat"
@@ -15,6 +19,9 @@ const selector = "uwsgibeat"
 type Uwsgibeat struct {
 	// UbConfig holds configurations of Uwsgibeat parsed by libbeat.
 	UbConfig ConfigSettings
+
+	done   chan uint
+	events publisher.Client
 
 	url    *url.URL
 	period time.Duration
@@ -55,11 +62,50 @@ func (ub *Uwsgibeat) Config(b *beat.Beat) error {
 
 // Setup Uwsgibeat.
 func (ub *Uwsgibeat) Setup(b *beat.Beat) error {
+	ub.events = b.Events
+	ub.done = make(chan uint)
+
 	return nil
 }
 
 // Run Uwsgibeat.
 func (ub *Uwsgibeat) Run(b *beat.Beat) error {
+	logp.Debug(selector, "Run uwsgibeat")
+
+	p := parser.NewStatsParser()
+
+	ticker := time.NewTicker(ub.period)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ub.done:
+			goto GotoFinish
+		case <-ticker.C:
+		}
+
+		start := time.Now()
+
+		s, err := p.Parse(*ub.url)
+		if err != nil {
+			logp.Err("Fail to read uWSGI stats: %v", err)
+			goto GotoNext
+		}
+		ub.events.PublishEvent(common.MapStr{
+			"@timestamp": common.Time(time.Now()),
+			"type":        "uwsgi",
+			"uwsgi":       s,
+		})
+
+	GotoNext:
+		end := time.Now()
+		duration := end.Sub(start)
+		if duration.Nanoseconds() > ub.period.Nanoseconds() {
+			logp.Warn("Ignoring tick(s) due to processing taking longer than one period")
+		}
+	}
+
+GotoFinish:
 	return nil
 }
 
@@ -70,4 +116,6 @@ func (ub *Uwsgibeat) Cleanup(b *beat.Beat) error {
 
 // Stop Uwsgibeat.
 func (ub *Uwsgibeat) Stop() {
+	logp.Debug(selector, "Stop uwsgibeat")
+	close(ub.done)
 }
